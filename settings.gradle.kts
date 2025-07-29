a4aa56ac1eb72e2204d485d0d49e8b61afb5756e
Rahil Chertara <rchertara@gmail.com>
2025-07-29 11:02:50 -0700

    it test added

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -118,6 +118,7 @@ dependencyResolutionManagement {
   repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
   repositories {
     mavenCentral()
+    mavenLocal()
     gradlePluginPortal()
   }
 }
